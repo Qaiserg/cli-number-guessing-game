2fc807f5b3d5d4444a9e5fd0b86d312bb0adebbd
Qaiser Gill <qaisermarcus2@gmail.com>
2024-04-16 13:24:43 +0500

    I have created a number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number Between 1-5: ",
+    },
+]);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log("Wahoo! you won.");
+}
+else {
+    console.log("Oops you lose! Better luck next time");
+}
